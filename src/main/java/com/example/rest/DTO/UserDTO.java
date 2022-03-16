8ebe6e7cf6ef1174d2a980ce9d2d9844493c6988
Андрей <90280844+Dron4000@users.noreply.github.com>
2022-03-16 19:46:36 +0300

    Update UserDTO.java

--- src/main/java/com/example/rest/DTO/UserDTO.java
+++ src/main/java/com/example/rest/DTO/UserDTO.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.Set;
 
-@JsonIgnoreProperties
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDTO {
 
     private Long id;
